6c94dd375736517f728bb997b8003ef0c6e0271f
Lucas Vasconcelos <lucasvasconcelos1202@gmail.com>
2026-04-06 16:22:24 -0300

    Initial commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-white text-4xl font-bold">
+        GymTracker 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
